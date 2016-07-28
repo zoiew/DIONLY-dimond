7e51a76472acf85c6b06c266c0ef888262d4a8dd
nicholas <15817168959@163.com>
2016-07-28 10:42:43 +0800

    登陆和注册cookie完成，07月28日

--- src/js/register.js
+++ src/js/register.js
@@ -14,12 +14,18 @@ $(function(){
 			var oError3 = $('.min_bto_p3');
 			var oError4 = $('.min_bto_p4');
 			var oError5 = $('.min_bto_p5');
+			//手机注册号码；
 			var sPname = oPname.val().replace(/ /g, "");
+			//注册密码；
 			var sUpass = oUpass.val();
 			var sCpass = oCpass.val();
-			oPname.value = sPname;
+			//定义满足提交条件；
+			var mobieNumTrue = false;//手机号码
+			var checkCodeTrue = false;//验证码；
+			var checkAgainTrue = false;//再次验证密码
+			var pswTrue = false;//密码；
 			var regName = /^(13[0-9]|14[0-9]|15[0-9]|18[0-9])[0-9]{8}$/;//13*/14*/15*/18*    11位		
-			var regPass = /^[\w]{6,20}$/;//密码只能包含数字、字母和下划线
+			var regPass = /^[\w]{6,20}$/;//密码只能包含数字、字母和下划线	
 			//验证手机
 			if(sPname.length==0){
 				oError1.html("手机号不能为空！");
@@ -27,11 +33,13 @@ $(function(){
 				 oError1.html("手机号格式有误！");
 			}else {
 				oError1.html("");
+				mobieNumTrue = true;
 			}
 			
 			//匹配输入的验证码是否与随机生成的验证码一致
 			if($('#checkCode').val()==$checkCodeSpan.html()){
 				oError2.html("");
+				checkCodeTrue = true;
 			}else{
 				 oError2.html("验证码输入有误！");
 			}
@@ -42,19 +50,34 @@ $(function(){
 				oError4.html("密码长度为6-20位,只能包含数字、字母和下划线");
 			}else {
 				oError4.html("");
+				pswTrue = true;
+				
 			}
 			//匹配两次输入的密码是否一致
 			if($('#checkPassword').val()==$('#userPassword').val()){
 				console.log($('#checkPassword').val());
 				console.log($('#userPassword').val());
 				oError5.html("");
+				checkAgainTrue = true;
+				
 			}else{
 				 oError5.html("两次输入的密码不一致！");
 			}
-		}	
+			//注册cookie验证；
+			var obj =new Object();
+			obj.number = sPname;
+			obj.password =sUpass;
+			var json = JSON.stringify(obj);
+			document.cookie = sPname+'='+json+';path/';
+			if(mobieNumTrue==true && checkCodeTrue==true && pswTrue==true&& checkAgainTrue==true){
+				location.href = 'login.html';		
+			}else{
+				alert('您输入有误，请重新填写');
+			}
+			
+			
+		}			
 	})
-
-
 	////登录验证随机生成验证码
 	fnLoginSwitch();
 	////登录验证点击换一张
